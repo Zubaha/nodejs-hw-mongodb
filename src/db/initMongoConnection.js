93c187cf288164af2dab28c205ca30e59a51be22
Zubaha <zubaha4052@gmail.com>
2024-10-14 17:16:21 +0200

    update

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,10 +5,10 @@ import mongoose from 'mongoose';
 export const initMongoConnection = async () => {
     const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
 
-    const mongoUri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+    const mongoUrl = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
     try {
-        await mongoose.connect(mongoUri);
+        await mongoose.connect(mongoUrl);
         console.log('Mongo connection successfully established!');
     } catch (error) {
         console.error('Mongo connection failed:', error.message);
