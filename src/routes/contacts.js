3f16276ba882f8b4a541f1f192def78f24e2cbbf
Zubaha <zubaha4052@gmail.com>
2024-10-25 17:18:24 +0200

    update

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import { getAllContacts, getContactById, createContact, updateContact, deleteContact } from '../controllers/contacts.js';
+import { isValidID } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { contactSchema } from '../validation/contacts.js';
 
 const router = express.Router();
 
 router.get('/', getAllContacts);
-router.get('/:contactId', getContactById);
-router.post('/', createContact);
-router.patch('/:contactId', updateContact);
-router.delete('/:contactId', deleteContact);
+router.get('/:contactId',isValidID, getContactById);
+router.post('/', validateBody(contactSchema), createContact);
+router.patch('/:contactId',isValidID, updateContact);
+router.delete('/:contactId',isValidID, deleteContact);
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,11 @@
+import Joi from 'joi';
+
+export const contactSchema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    phoneNumber: Joi.string().min(3).max(20).required(),
+    email: Joi.string().email().optional(),
+    isFavourite: Joi.boolean().optional(),
+    contactType: Joi.string().valid('work', 'home', 'personal').optional(),
+});
+
+
