81d5897c1a70430bcf3db96d347a325463e44895
Zubaha <zubaha4052@gmail.com>
2024-10-31 11:51:04 +0100

    update

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -12,6 +12,8 @@ export const contactPatchSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
     email: Joi.string().email().optional(),
+    isFavourite: Joi.boolean().optional(),
+    contactType: Joi.string().valid('work', 'home', 'personal').optional(),
 });
 
 
