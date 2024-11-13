d3b114d62553df15e279fd6b155947eba8eefde2
Zubaha <zubaha4052@gmail.com>
2024-11-13 14:22:03 +0200

    update

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/auth.js';
 import {errorHandler} from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
     const app = express();
@@ -17,12 +18,11 @@ export const setupServer = () => {
 
     app.use('/contacts', contactsRoutes);
     app.use('/auth', authRoutes);
+    app.use('/api-docs', swaggerDocs());
     app.use(notFoundHandler);
     app.use(errorHandler);
 
-    // app.use((req, res) => {
-    //     res.status(404).json({ message: 'Not found' });
-    // });  
+    
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
